8051f6d18836aaa2505b5d2c8e85f90e3905ba59
한예주 <yejoo_han@college.harvard.edu>
2023-02-14 09:03:25 -0500

    Added more words to lexicon

--- lexicon.ml
+++ lexicon.ml
@@ -14,8 +14,11 @@ type token =
   | Construct of construct
   | Whansung
   | Yejoo
+  | Yejoos
+  | Mother
   | Left
   | Saw
+  | Thought
 
 type tokens =
   | Single of token
@@ -25,25 +28,34 @@ let str_to_token (str : string) : token =
   match str with
   | "whansung" -> Whansung
   | "yejoo" -> Yejoo
+  | "yejoo's" -> Yejoos
+  | "mother" -> Mother
   | "left" -> Left
   | "saw" -> Saw
-  | _ -> failwith "couldn't match string to token" ;;
+  | "thought" -> Thought
+  | _ -> failwith "couldn't match string to token"
 
 let token_to_str (tok : token) : string =
   match tok with
   | Construct construct -> construct_to_str construct
   | Whansung -> "whansung"
   | Yejoo -> "yejoo"
+  | Yejoos -> "yejoo's"
+  | Mother -> "mother"
   | Left -> "left"
-  | Saw -> "saw" ;;
+  | Saw -> "saw"
+  | Thought -> "thought"
 
 let token_to_type (tok : token) : semantic_type =
   match tok with
   | Construct _ -> Construct
   | Whansung -> Element
   | Yejoo -> Element
+  | Yejoos -> Element
+  | Mother -> Backward(Element, Element)
   | Left -> Backward(Element, Truth)
   | Saw -> Forward(Element, Backward(Element, Truth))
+  | Thought -> Forward(Truth, Backward(Element, Truth))
 
 let rec tokens_to_str (toks : tokens) : string =
   match toks with
