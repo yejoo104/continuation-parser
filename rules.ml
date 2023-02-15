f68c59dce0ef7cdc1ffd0d388d517f7e87a76536
한예주 <yejoo_han@college.harvard.edu>
2023-02-15 18:36:13 -0500

    added skip rules and nested continuations. integrated with multiple interpretation framework

--- rules.ml
+++ rules.ml
@@ -55,27 +55,64 @@ let apply_all_rules ((tok1, type1): tokens * semantic_type) ((tok2, type2): toke
   match type1, type2 with
   (* Putting this at front for ease of pattern-matching. Clean up a little bit afterwards *)
   (* TODO: DEFINITELY CLEAN THIS SHIT UP *)
+  (* TODO: resolve Empty type in a consistent way *)
+  (* Rule 11 *)
+  | Function(Continuation(t1, t2), t3), Function(Continuation(Function(Continuation(Backward(t4, t5), t6), t7), t8), t9) ->
+    (match (equivalent_type t1 t4), (equivalent_type t2 t7) with
+     | Some t1, Some t2 ->
+       Some [List [Single (Construct META); List [Single (Construct META); Single (Construct BACKWARD)]; List [Single (Construct LIFT); tok1]; tok2], Function(Continuation(Function(Continuation(t5, t6), t3), t8), t9)]
+     | None, _ | _, None -> None)
+  (* Rule 12 *)
+  | Function(Continuation(Function(Continuation(t1, t2), t3), t4), t5), Function(Continuation(Backward(t6, t7), t8), t9) ->
+    (match (equivalent_type t1 t6), (equivalent_type t2 t9) with
+     | Some t1, Some t2 ->
+       Some [List [Single (Construct META); List [Single (Construct META); Single (Construct BACKWARD)]; tok1; List [Single (Construct LIFT); tok2]], Function(Continuation(Function(Continuation(t7, t8), t3), t4), t5)]
+     | None, _ | _, None -> None)
+  (* Rule 13 *)
+  | Function(Continuation(Forward(t1, t2), t3), t4), Function(Continuation(Function(Continuation(t5, t6), t7), t8), t9) ->
+    (match (equivalent_type t1 t5), (equivalent_type t3 t7) with
+     | Some t1, Some t3 ->
+       Some [List [Single (Construct META); List [Single (Construct META); Single (Construct FORWARD)]; List [Single (Construct LIFT); tok1]; tok2], Function(Continuation(Function(Continuation(t2, t6), t4), t8), t9)]
+     | None, _ | _, None -> None
+    )
+  (* Rule 14 *)
+  | Function(Continuation(Function(Continuation(Forward(t1, t2), t3), t4), t5), t6), Function(Continuation(t7, t8), t9) ->
+    (match (equivalent_type t1 t7), (equivalent_type t3 t9) with
+     | Some t1, Some t3 ->
+       Some [List [Single (Construct META); List [Single (Construct META); Single (Construct FORWARD)]; tok1; List [Single (Construct LIFT); tok2]], Function(Continuation(Function(Continuation(t2, t8), t4), t5), t6)]
+     | None, _ | _, None -> None
+    )
+  (* Rule 7 *)
   | Function(Continuation(t1, t2), t3), Function(Continuation(Backward(t4, t5), t6), t7) ->
     if t1 = t4 && t2 = t7 then Some [List [Single (Construct META); Single (Construct BACKWARD); tok1; tok2], Function(Continuation(t5, t6), t3)]
     else None
+  (* Rule 8 *)
   | Function(Continuation(Forward(t1, t2), t3), t4), Function(Continuation(t5, t6), t7) ->
     if t1 = t5 && t3 = t7 then Some [List [Single (Construct META); Single (Construct FORWARD); tok1; tok2], Function(Continuation(t2, t6), t4)]
     else None
+  (* Rule 5 *)
   | t1, Function(Continuation(Backward(t2, t3), t4), t5) ->
     if t1 = t2 then Some [List [Single (Construct META); Single (Construct BACKWARD); List [Single (Construct LIFT); tok1]; tok2], Function(Continuation(t3, t4), t5)]
     else None
+  (* Rule 6 *)
   | Function(Continuation(Forward(t1, t2), t3), t4), t5 ->
     if t1 = t5 then Some [List [Single (Construct META); Single (Construct FORWARD); tok1; List [Single (Construct LIFT); tok2]], Function(Continuation(t2, t3), t4)]
     else None
+  (* Rule 3, 10 *)
   | Function(Continuation(t1, t2), t3), Backward(t4, t5) ->
-    if t1 = t4 then Some [List [Single (Construct META); Single (Construct BACKWARD); tok1; List [Single (Construct LIFT); tok2]], Function(Continuation(t5, t2), t3)]
+    if t1 = t4 then Some [List [Single (Construct META); Single (Construct BACKWARD); tok1; List [Single (Construct LIFT); tok2]], Function(Continuation(t5, t2), t3);
+                          List [Single (Construct META); List [Single (Construct META); Single (Construct BACKWARD)]; List [Single (Construct SKIP); Single (Construct LIFT); tok1]; List [Single (Construct LIFT); List [Single (Construct LIFT); tok2]]], Function(Continuation(Function(Continuation(t2, Empty), Empty), t4), t5)]
     else None
+  (* Rule 4, 9 *)
   | Forward(t1, t2), Function(Continuation(t3, t4), t5) ->
-    if t1 = t3 then Some [List [Single (Construct META); Single (Construct FORWARD); List [Single (Construct LIFT); tok1]; tok2], Function(Continuation(t2, t4), t5)]
+    if t1 = t3 then Some [List [Single (Construct META); Single (Construct FORWARD); List [Single (Construct LIFT); tok1]; tok2], Function(Continuation(t2, t4), t5);
+                          List [Single (Construct META); List [Single (Construct META); Single (Construct FORWARD)]; List [Single (Construct LIFT); List [Single (Construct LIFT); tok1]]; List [Single (Construct SKIP); Single (Construct LIFT); tok2]], Function(Continuation(Function(Continuation(t2, Empty), Empty), t4), t5)]
     else None
+  (* Rule 1 *)
   | t1, Backward(t2, t3) ->
     if t1 = t2 then Some [List [Single (Construct BACKWARD); tok1; tok2], t3]
     else None
+  (* Rule 2 *)
   | Forward(t1, t2), t3 ->
     if t1 = t3 then Some [List [Single (Construct FORWARD); tok1; tok2], t2]
     else None
--- types.ml
+++ types.ml
@@ -1,8 +1,15 @@
 type semantic_type =
   | Construct (* This is for convenience because we can't represent polymorphism yet *)
+  | Empty (* undetermined type *)
   | Element
   | Truth
   | Forward of semantic_type * semantic_type (* / operator *)
   | Backward of semantic_type * semantic_type (* \ operator *)
   | Continuation of semantic_type * semantic_type (* ~ operator *)
   | Function of semantic_type * semantic_type (* -> operator *)
+
+let equivalent_type (t1 : semantic_type) (t2 : semantic_type) : semantic_type option =
+  match t1, t2 with
+  | Empty, t
+  | t, Empty -> Some t
+  | t1, t2 -> if t1 = t2 then Some t1 else None
